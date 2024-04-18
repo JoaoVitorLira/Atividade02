c39245e58ddd791780322cc94b46f11c2a17dc16
João Lira <joao.liraves07@gmail.com>
2024-04-18 15:03:18 -0300

    Initial commit

---
+++ Atividade02.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+
+//unidades, é cobrado R$1.50 por unidade, acima disso, é cobrado R$1.25. Faça
+//que calcule quanto um determinado cliente irá pagar
+
+//int main(int argc, char* argv[]) {
+//	int quantidade = 22;
+//	float preco = 1.50;
+//	float preco2 = 1.25;
+//
+//	double valor1 = quantidade * preco;
+//	double valor2 = quantidade * preco2;
+//
+//	if (quantidade <= 25) {
+//		cout << "Valor a ser pago: ";
+//		cout << valor1;
+//	}
+//	else if (quantidade > 25) {
+//		cout << "Valor a ser pago: ";
+//		cout << valor2;
+//	}
+//	
+//	return 0;
+//}
+
+
+//2 – Declare uma variável com um número, se ele for maior que 10, faça o
+//cálculo do número + 10 * 2. Senão faça número + 9 * 3 – 8
+
+//int main(int argc, char* argv[]) {
+//	double variavel = 17;
+//			
+//	double calc1 = variavel + 10 * 2;
+//	double calc2 = variavel + 9 * 3 - 8;
+//
+//	if (variavel > 10) {
+//		cout << calc1;
+//	}
+//	else {
+//		cout << calc2;
+//	}
+// return 0;
+//}
+
+
+//3 – Faça um código que verifique se um número é positivo ou negativo
+
+//int main(int argc, char* argv[]) {
+//	int numero = -3;
+//
+//	if (numero >= 0) {
+//		cout << "Esse numero e positivo";
+//	}
+//	else {
+//		cout << "Esse numero e negativo";
+//	}
+//
+//	return 0;
+//}
+
+//4 – Faça um codigo que tenha duas variáveis numéricas e mostre qual delas é
+//a maior
+
+//int main(int argc, char* argv[]) {
+//	int var1 = 150;
+//	int var2 = 15;
+//
+//	if (var1 > var2) {
+//		cout << "O primeiro numero e maior";
+//	}
+//	else if (var2 > var1) {
+//		cout << "O segundo numero e maior";
+//	}
+//	else if (var1 = var2) {
+//		cout << "Os dois numeros sao iguais";
+//	}
+//
+//	return 0;
+//}
+
+//5 – Um professor deseja um sistema para o cálculo de média dos seus alunos,
+//para isso, declare duas variáveis para as notas, realize o cálculo da média e
+//tre, se a média for maior ou igual a 7 mostre “Aprovado”, senão mostre
+//“Reprovado”
+
+//int main(int argc, char* argv[]) {
+//	double nota1 = 7;
+//	double nota2 = 10;
+//
+//	double media = (nota1 + nota2) / 2;
+//
+//	if (media >= 7) {
+//		cout << "Aprovado!";
+//	}
+//	else {
+//		cout << "Reprovado";
+//	}
+//
+//	return 0;
+//}
